9dcddddde095316c46c3fc85a06b4d2bf4e6821c
Stefan Kögl <stefan.koegl@rub.de>
2014-03-09 21:23:09 +0100

    added copy right notice

--- osc2cam/osc2cam/testing.py
+++ osc2cam/osc2cam/testing.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 # This file is part of chaosc
@@ -15,8 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with chaosc.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright (C) 2012-2013 Stefan Kögl
-
+# Copyright (C) 2014 Stefan Kögl
 
 import socket, argparse, sys
 
