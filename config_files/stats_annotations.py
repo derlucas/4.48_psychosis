e5203efcb1d7e0955695488c26653508c5ece918
Stefan Kögl <stefan.koegl@rub.de>
2014-03-19 21:32:29 +0100

    added all remaining used OSCMessages for psychosis

--- config_files/stats_annotations.py
+++ config_files/stats_annotations.py
@@ -1,2 +1,6 @@
+/(.*?)/airFlow; i; amplitude
+/(.*?)/temperature; i; temperature
+/(.*?)/heartbeat; i, i, i; on/off, heart rate, oxgen_level
 /(.*?)/ekg; i; amplitude
+/(.*?)/emg; i; amplitude
 /plot/(.*?); i; on/off
