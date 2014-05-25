05745e3c52819f4638d6f38ce9b38735529d41f2
Stefan Kögl <stefan.koegl@rub.de>
2014-05-25 15:53:50 +0200

    consolidate mjpeg streaming server using qtnetwork into a reusable class

---
+++ psylib/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from distribute_setup import use_setuptools
+use_setuptools()
+
+import sys
+from setuptools import find_packages, setup
+
+if sys.version_info >= (3,):
+    extras['use_2to3'] = True
+
+setup(
+    name='psylib',
+    version="0.1",
+    packages=find_packages(exclude=["scripts",]),
+
+    include_package_data = True,
+
+    exclude_package_data = {'': ['.gitignore']},
+
+    zip_safe = False,
+
+    # pypi metadata
+    author = "Stefan Kögl",
+
+    # FIXME: add author email
+    author_email = "hotte@ctdo.de",
+    description = "library for psychosis",
+
+    # FIXME: add long_description
+    long_description = """
+    """,
+
+    # FIXME: add license
+    license = "GPL",
+
+    # FIXME: add keywords
+    keywords = "",
+
+    # FIXME: add download url
+    url = "",
+)
